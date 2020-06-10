2499324a8036fe82d2b346cce854514fdde89560
Dev1 <Dev1@gmail.com>
2020-06-10 17:10:33 +0530

    creating repository implementation class

---
+++ StudentAppDemo/src/main/java/com/example/repositoryImpl/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.example.repositoryImpl;
+
+import com.example.Entity.Student;
+
+public class StudentRepository implements com.example.repository.StudentRepository {
+
+	@Override
+	public Integer studentSave(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
